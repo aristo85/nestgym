ffd8bf54868d395517c42a8f333c254e3a79d5ab
Ari Rahim <ari.r@itfox-web.com>
2021-01-03 00:26:48 +0700

    add Content-Range to header

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -30,7 +30,7 @@ import { UsersService } from './users.service';
       // get all apps in the db
       const list = await this.userService.findAll();
       const count = list.length;
-      req.res.set('x-total-count', `${count}`)
+      req.res.set('Content-Range', `0-${count}/${count}`)
       return list;
     }
   
