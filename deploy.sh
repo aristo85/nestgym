1fc0a6b31dfac3ea67aba93e993851394ae9f3bb
Vladas.z <vladas.z@itfox-web.com>
2021-02-08 10:28:18 +0300

    Исправлен деплой

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-git checkout $1 && git pull origin $1 &&
-git submodule init && git submodule update && cd getcoachadmin/ && git checkout $1 && git pull origin $1 &&
+(
+    git checkout $1 && git pull origin $1 &&
+    git submodule init && git submodule update && cd getcoachadmin/ && git checkout $1 && git pull origin $1 &&
 
-cd .. && 
-docker-compose up -d --build &&
-docker system prune -f &&
-exit
+    cd .. && 
+    docker-compose up -d --build &&
+    docker system prune -f &&
+    exit
+) &
+disown %1
